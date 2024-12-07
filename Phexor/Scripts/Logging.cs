dab7670f3e04392267b43d1761d3d13ab34504c9
Cliplicht <git@cliplicht.de>
2024-12-07 20:31:02 +0100

    Ugly fix for LogFileName Changed SelfContained

--- Phexor/Scripts/Logging.cs
+++ Phexor/Scripts/Logging.cs
@@ -12,11 +12,12 @@ namespace Phexor.Scripts;
 // M. = Method
 // Xc. = XAML code
 // C#c. = C# code
-public class Logging
+public static class Logging
 {
     public static string AppdataFolder = Path.Combine((Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)), "Phexor"); //C. V. and get Locatin of Appdata Folder for Phexor
     public static string LogDirectory = Path.Combine(AppdataFolder, "Logs"); //C. V. and get Locatin of LogDirectory in Appdata Folder for Phexor
-    public static string DailyLogFile = Path.Combine(AppdataFolder, LogDirectory + @"\" + DateTime.Today.ToString("d") + ".log"); //C. V. and get Locatin of DailyLogFile in LogDirectory in Appdata Folder for Phexor
+    public static string DailyLogFilePath = Path.Combine(AppdataFolder, LogDirectory); //C. V. and get Locatin of DailyLogFile in LogDirectory in Appdata Folder for Phexor
+    private static string DailyLogFileName = string.Concat(DailyLogFilePath, "\\",DateTime.Today.ToString("d").Replace("/",".")) + ".log";
     public static int LogCount; //C. V. for RemoveOldLogFile M.
     public static void Log(string Log, string Identifikator) //M. To Add a line to the Logs
     {
@@ -24,7 +25,8 @@ public class Logging
         {
             Directory.CreateDirectory(LogDirectory); //C. Log Directory
         }
-        using (StreamWriter writer = new StreamWriter(DailyLogFile, true, Encoding.UTF8)) //C. or Accesses the DailyLogFile
+
+        using (StreamWriter writer = new StreamWriter(DailyLogFileName, true, Encoding.UTF8)) //C. or Accesses the DailyLogFile
         {
             string LogText = $"{DateTime.Now:HH:mm:ss} [{Identifikator}] : {Log}"; //C. the LogText
             writer.WriteLine(LogText); //Add new LogText to the LogFile
@@ -38,7 +40,7 @@ public class Logging
         {
             Directory.CreateDirectory(LogDirectory); //C. Log Directory
         }
-        using (StreamWriter writer = new StreamWriter(DailyLogFile, true, Encoding.UTF8)) //C. or Accesses the DailyLogFile
+        using (StreamWriter writer = new StreamWriter(DailyLogFileName, true, Encoding.UTF8)) //C. or Accesses the DailyLogFile
         {
             string LogText = $"{DateTime.Now:HH:mm:ss} [{Identifikator}] | Catch | : {Log}"; //C. the LogText
             writer.WriteLine(LogText); //Add new LogText to the LogFile
