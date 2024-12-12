be8039464337d40d8fb7a6005e86392fecc4d2f5
Cliplicht <git@cliplicht.de>
2024-12-12 17:15:31 +0100

    removed ToDo

--- Phexor/Converters/EmptyStringToVisibilityConverter.cs
+++ Phexor/Converters/EmptyStringToVisibilityConverter.cs
@@ -7,13 +7,13 @@ namespace Phexor.Converters;
 
 public class EmptyStringToVisibilityConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return value is Visibility and Visibility.Visible ? string.Empty : null;
     }
 }
