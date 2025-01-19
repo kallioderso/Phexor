96c2724dd49c708f5fe6ff458d9fa6d41e0a5d9a
kallioderso <pascal.loesch.coburg@gmail.com>
2025-01-19 11:04:04 +0100

    Update some Settings

--- Phexor/Settings.xaml.cs
+++ Phexor/Settings.xaml.cs
@@ -11,26 +11,31 @@ public partial class Settings
         explorerRef = explorer;
         InitializeComponent();
         SettingMenu.Content = new SettingMenus.ColorSettings();
+        Text2.Text = "Color Settings";
     }
 
     private void ColorSettingsButton_OnClick(object sender, RoutedEventArgs e)
     {
         SettingMenu.Content = new SettingMenus.ColorSettings();
+        Text2.Text = "Color Settings";
     }
 
     private void SymbolSettingsButton_OnClick(object sender, RoutedEventArgs e)
     {
         SettingMenu.Content = new SettingMenus.SymbolSettings();
+        Text2.Text = "Symbol Settings";
     }
     
     private void SizesSettingsButton_OnClick(object sender, RoutedEventArgs e)
     {
         SettingMenu.Content = new SettingMenus.SizeSettings();
+        Text2.Text = "Size Settings";
     }
     
     private void LoggingSettingsButton_OnClick(object sender, RoutedEventArgs e)
     {
         SettingMenu.Content = new SettingMenus.LoggingSettings();
+        Text2.Text = "Logging Settings";
     }
 
     private void Settings_OnClosed(object sender, EventArgs e)
