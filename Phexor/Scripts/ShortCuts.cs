571843a148c0b6c5858498fe575c57bc1046bba4
kallioderso <pascal.loesch.coburg@gmail.com>
2025-04-26 15:13:32 +0200

    Cleaning Up ShortCuts.cs

--- Phexor/Scripts/ShortCuts.cs
+++ Phexor/Scripts/ShortCuts.cs
@@ -5,37 +5,49 @@ using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Phexor.Scripts;
+// C. = Create
+// G. = Generate
+// V. = Variable
+// L. = List
+// M. = Method
+// Xc. = XAML code
+// C#c. = C# code
 
 public static class ShortCuts
 {
-    public static void KeyPress(KeyEventArgs e, Explorer explorer) => Selector(e, explorer);
+    //-----Public Methods-----\\
+    public static void KeyPress(KeyEventArgs e, Explorer explorer) => Selector(e, explorer); //Public Method to call the Selector method
     
-    private static void Selector(KeyEventArgs e, Explorer explorer)
+    //-----Private Methods-----\\
+    private static void Selector(KeyEventArgs e, Explorer explorer) //Selector method to check which key was pressed
     {
-        if (Keyboard.FocusedElement is TextBox)
+        if (Keyboard.FocusedElement is TextBox) //Check if the focused element is a TextBox
         {
-            switch (e.Key)
+            switch (e.Key) //Check which key was pressed
             {
-                case Key.Enter: PathInputEnter(explorer); break;
-                case Key.Escape: EscapeInput(explorer); break;
+                case Key.Enter: PathInputEnter(explorer); break; //Enter key pressed
+                case Key.Escape: EscapeInput(explorer); break; //Escape key pressed
             }
         }
         else
         {
-            switch (e.Key)
+            switch (e.Key) //Check which key was pressed
             {
-                case Key.U: Undo(explorer); break;
-                case Key.R: Redo(explorer); break;
-                case Key.I: Input(explorer); break;
-                case Key.S: Settings(explorer); break;
+                case Key.E: explorer.Clear(); break; //E key pressed
+                case Key.F: explorer.Clear(); break; //F key pressed
+                case Key.O: explorer.Clear(); break; //O key pressed
+                case Key.C: explorer.Clear(); break; //C key pressed
+                case Key.X: explorer.Clear(); break; //X key pressed
+                case Key.Z: explorer.Clear(); break; //Z key pressed
             }
         }
     }
     
-    private static void Undo(Explorer e) => e.UndoPath();
-    private static void Redo(Explorer e) => e.RedoPath();
-    private static void Input(Explorer e) => Keyboard.Focus(e.PathInput);
-    private static void PathInputEnter(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true; e.InputFieldPath();}
-    private static void EscapeInput(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true;}
-    private static void Settings(Explorer e) => e.Settings();
+    //-----ShortCut Methods-----\\
+    private static void Undo(Explorer e) => e.UndoPath(); //call the UndoPath method from the Explorer class
+    private static void Redo(Explorer e) => e.RedoPath(); //call the RedoPath method from the Explorer class
+    private static void Input(Explorer e) => Keyboard.Focus(e.PathInput); //Set focus to the PathInput TextBox
+    private static void PathInputEnter(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true; e.InputFieldPath(); } //call the PathInputEnter method from the Explorer class
+    private static void EscapeInput(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true;} //Set focus to the PathInput TextBox
+    private static void Settings(Explorer e) => e.Settings(); //call the Settings method from the Explorer class
 }
