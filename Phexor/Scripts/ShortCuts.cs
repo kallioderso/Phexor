703b6f058f63a3b36fbef2e8d46851298d7e09f6
kallioderso <pascal.loesch.coburg@gmail.com>
2025-04-26 15:27:03 +0200

    Fix in ShortCuts.cs

--- Phexor/Scripts/ShortCuts.cs
+++ Phexor/Scripts/ShortCuts.cs
@@ -33,21 +33,19 @@ public static class ShortCuts
         {
             switch (e.Key) //Check which key was pressed
             {
-                case Key.E: explorer.Clear(); break; //E key pressed
-                case Key.F: explorer.Clear(); break; //F key pressed
-                case Key.O: explorer.Clear(); break; //O key pressed
-                case Key.C: explorer.Clear(); break; //C key pressed
-                case Key.X: explorer.Clear(); break; //X key pressed
-                case Key.Z: explorer.Clear(); break; //Z key pressed
+                case Key.U: Undo(explorer); break; //U key pressed
+                case Key.R: Redo(explorer); break; //R key pressed
+                case Key.I: Input(explorer); break; //I key pressed
+                case Key.S: Settings(explorer); break; //S key pressed
             }
         }
     }
     
     //-----ShortCut Methods-----\\
-    private static void Undo(Explorer e) => e.UndoPath(); //call the UndoPath method from the Explorer class
-    private static void Redo(Explorer e) => e.RedoPath(); //call the RedoPath method from the Explorer class
-    private static void Input(Explorer e) => Keyboard.Focus(e.PathInput); //Set focus to the PathInput TextBox
-    private static void PathInputEnter(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true; e.InputFieldPath(); } //call the PathInputEnter method from the Explorer class
-    private static void EscapeInput(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true;} //Set focus to the PathInput TextBox
-    private static void Settings(Explorer e) => e.Settings(); //call the Settings method from the Explorer class
+    private static void Undo(Explorer e) => e.UndoPath(); //Undo the last action
+    private static void Redo(Explorer e) => e.RedoPath(); //Redo the last action
+    private static void Input(Explorer e) => Keyboard.Focus(e.PathInput); //Focus on the PathInput TextBox
+    private static void PathInputEnter(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true; e.InputFieldPath();} //Call the PathInputEnter method
+    private static void EscapeInput(Explorer e) { Keyboard.ClearFocus(); e.PathInput.Focusable = false; e.Focus(); e.PathInput.Focusable = true;} //Clear the focus from the PathInput TextBox
+    private static void Settings(Explorer e) => e.Settings(); //Open the Settings window
 }
