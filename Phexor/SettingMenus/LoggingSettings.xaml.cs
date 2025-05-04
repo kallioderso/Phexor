05ec17ba585f5556ad598fa19f2d7aa31da2d755
kallioderso <pascal.loesch.coburg@gmail.com>
2025-05-04 11:09:38 +0200

    Cleaned Up LoggingSettings.xaml.cs and SizeSettings.xaml.cs

--- Phexor/SettingMenus/LoggingSettings.xaml.cs
+++ Phexor/SettingMenus/LoggingSettings.xaml.cs
@@ -9,89 +9,29 @@ namespace Phexor.SettingMenus;
 
 public partial class LoggingSettings : UserControl
 {
-    public LoggingSettings()
+    public LoggingSettings() { InitializeComponent(); GetValues(); }
+    private void SwitchContent(object sender, RoutedEventArgs e) //M. to switch the state of the Buttons
     {
-        InitializeComponent();
-        GetValues();
+        var setting = sender as Button; //Get the sender of the event
+        if (setting.Content.ToString() == " ") { setting.Content = "X"; SaveSettings(setting, true); } //if the state is off than switch it to on
+        else if (setting.Content.ToString() == "X") { setting.Content = " "; SaveSettings(setting, false); } //if the state is on than switch it to off
     }
     
-    private void SwitchContent(object sender, RoutedEventArgs e)
+    private void SaveSettings(object sender, bool state) //M. to save the settings
     {
-        var setting = sender as Button;
-        if (setting.Content.ToString() == " ")
-        {
-            setting.Content = "X";
-            SaveSettings(setting, true);
-        }
-        else if (setting.Content.ToString() == "X")
-        {
-            setting.Content = " ";
-            SaveSettings(setting, false);
-        }
+        if (sender == ExplorerLog) { if (state) SettingsControl.Log2 = 1; else SettingsControl.Log2 = 0; } //if Sender is ExplorerLog Button save the state
+        else if (sender == SettingsLog) { if (state) SettingsControl.Log3 = 1; else SettingsControl.Log3 = 0; } //if Sender is SettingsLog Button save the state
+        else if (sender == ScriptsLog) { if (state) SettingsControl.Log4 = 1; else SettingsControl.Log4 = 0; } //if Sender is ScriptsLog Button save the state
+        else if (sender == LogCountSlider) SettingsControl.Log1 = LogCountSlider.Value;  //if Sender is LogCountSlider save the state
+        SettingsControl.SetSettings(); //Save the Settings into Appdata
     }
-    
-    private void SaveSettings(object sender, bool state)
+    private void LogCountChanged(object sender, MouseEventArgs mouseEventArgs) => SaveSettings(LogCountSlider, true); //Save the settings when the slider is moved
+    private void GetValues() //Get the values from the settings
     {
-        if (sender == ExplorerLog)
-        {
-            if (state)
-            {
-                SettingsControl.Log2 = 1;
-            }
-            else
-            {
-                SettingsControl.Log2 = 0;
-            }
-        }
-        else if (sender == SettingsLog)
-        {
-            if (state)
-            {
-                SettingsControl.Log3 = 1;
-            }
-            else
-            {
-                SettingsControl.Log3 = 0;
-            }
-        }
-        else if (sender == ScriptsLog)
-        {
-            if (state)
-            {
-                SettingsControl.Log4 = 1;
-            }
-            else
-            {
-                SettingsControl.Log4 = 0;
-            }
-        }
-        else if (sender == LogCountSlider)
-        {
-            SettingsControl.Log1 = LogCountSlider.Value;
-        }
-        SettingsControl.SetSettings();
-    }
-
-    private void LogCountChanged(object sender, MouseEventArgs mouseEventArgs)
-    {
-        SaveSettings(LogCountSlider, true);
-    }
-
-    private void GetValues()
-    {
-        SettingsControl.GetSettings();
-        LogCountSlider.Value = SettingsControl.Log1;
-        if (SettingsControl.Log2 == 1)
-        {
-            ExplorerLog.Content = "X";
-        }
-        if (SettingsControl.Log3 == 1)
-        {
-            SettingsLog.Content = "X";
-        }
-        if (SettingsControl.Log4 == 1)
-        {
-            ScriptsLog.Content = "X";
-        }        
+        SettingsControl.GetSettings(); //Get Saved Settings
+        LogCountSlider.Value = SettingsControl.Log1; //Get Value for Log Count
+        if (SettingsControl.Log2 == 1) ExplorerLog.Content = "X"; //Get Value for Explorer Log button
+        if (SettingsControl.Log3 == 1) SettingsLog.Content = "X"; //Get Value for Settings Log button
+        if (SettingsControl.Log4 == 1) ScriptsLog.Content = "X"; //Get Value for Scripts Log button
     }
 }
--- Phexor/SettingMenus/SizeSettings.xaml.cs
+++ Phexor/SettingMenus/SizeSettings.xaml.cs
@@ -4,32 +4,25 @@ using System.Windows.Controls;
 using Phexor.Scripts;
 
 namespace Phexor.SettingMenus;
+// C. = Create
+// G. = Generate
+// V. = Variable
+// L. = List
+// M. = Method
+// Xc. = XAML code
+// C#c. = C# code
 
 public partial class SizeSettings : UserControl
 {
-    public SizeSettings()
-    {
-        InitializeComponent();
-        GetValues();
-    }
+    //-----Constructor-----\\
+    public SizeSettings() { InitializeComponent(); GetValues(); } //Constructor to initialize the settings
 
-    private void GetValues()
+    //-----Private Methods-----\\
+    private void GetValues() { SettingsControl.GetSettings(); HightSize.Value = SettingsControl.Size1; TextSize.Value = SettingsControl.Size2; } //Get the values from the settings
+    private void SafeSettings(object sender, RoutedEventArgs e) //M. to save the settings
     {
-        SettingsControl.GetSettings();
-        HightSize.Value = SettingsControl.Size1;
-        TextSize.Value = SettingsControl.Size2;
-    }
-
-    private void SafeSettings(object sender, RoutedEventArgs e)
-    {
-        if (sender == HightSize)
-        {
-            SettingsControl.Size1 = Convert.ToInt32(HightSize.Value);
-        }
-        else if (sender == TextSize)
-        {
-            SettingsControl.Size2 = Convert.ToInt32(TextSize.Value);
-        }
-        SettingsControl.SetSettings();
+        if (sender == HightSize) { SettingsControl.Size1 = Convert.ToInt32(HightSize.Value); } //Set the size of the directorys and files
+        else if (sender == TextSize) { SettingsControl.Size2 = Convert.ToInt32(TextSize.Value); } //Set the size of the text
+        SettingsControl.SetSettings(); //Save the Settings into Appdata
     }
 }
